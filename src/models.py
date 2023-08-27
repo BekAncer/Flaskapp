65c45c371877d757494df3fb62b4852938b67285
Bek_Ancer <89043156+BekAncer@users.noreply.github.com>
2023-08-27 18:09:20 +0600

    Update models.py

--- src/models.py
+++ src/models.py
@@ -20,7 +20,7 @@ class Sneaker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    imgsrc = db.Column(db.String(100),nullable=False)
+    imgsrc = db.Column(db.String(100), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __repr__(self):
