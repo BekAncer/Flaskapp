bd09c3496b6ee6db9a29b9f3168f383aa7eba94c
Bek_Ancer <89043156+BekAncer@users.noreply.github.com>
2023-08-27 18:03:42 +0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
-from models import DB_URI,Sneaker,db
+from models import DB_URI, Sneaker, db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
@@ -11,21 +11,21 @@ db.init_app(app)
 
 
 menu = [
-    {"name":"Добавить","url":"/CatalogChange"},
-    {"name":"Убрать","url":"/CatalogDelete"},
-    {"name":"Каталог","url":"/SneakerCatalog"},
-    {"name":"Контакты","url":"/Contacts"},
+    {"name": "Добавить", "url": "/CatalogChange"},
+    {"name": "Убрать", "url": "/CatalogDelete"},
+    {"name": "Каталог", "url": "/SneakerCatalog"},
+    {"name": "Контакты", "url": "/Contacts"},
 ]
 
 
 @app.route("/")
 @app.route("/Contacts")
-def Contacts():
-    return render_template('Contacts.html',Menu=menu)
+def contacts():
+    return render_template('Contacts.html', Menu=menu)
 
 
 @app.route("/CatalogChange", methods=["POST", "GET"])
-def CatalogChange():
+def catalog_change():
     if request.method == "POST":
         sneaker = Sneaker(
             name=request.form["name"],
@@ -34,27 +34,26 @@ def CatalogChange():
         )
         db.session.add(sneaker)
         db.session.commit()
-    return render_template('CatalogChange.html',Menu=menu)
+    return render_template('CatalogChange.html', Menu=menu)
 
 
 @app.route("/CatalogDelete", methods=["POST", "GET"])
-def DeleteSneaker():
+def delete_sneaker():
     if request.method == "POST":
         snkr = Sneaker.query.filter_by(name=request.form['name']).first()
         if snkr:
             db.session.delete(snkr)
             db.session.commit()
-    return render_template('CatalogDelete.html',Menu=menu)
+    return render_template('CatalogDelete.html', Menu=menu)
 
 
 @app.route("/SneakerCatalog")
 def about():
     Sneakers = db.session.execute(db.select(Sneaker).order_by(Sneaker.name)).scalars()
-    return render_template('Catalog.html',Sneakers=Sneakers,Menu=menu)
+    return render_template('Catalog.html', Sneakers=Sneakers, Menu=menu)
 
 
 if __name__ == '__main__':
     app.run(
-        host='0.0.0.0',port='5001',
-        debug=True
+        host='0.0.0.0', port='5001'
     )
